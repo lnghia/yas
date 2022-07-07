16ab918ef3abf137e95653bc1ee845d6cf349a91
n3w-us3r <trongnghialedinh@gmail.com>
2022-07-07 22:17:57 +0700

    define JwtDecoder bean

--- product/src/main/java/com/yas/product/configurations/SecurityConfig.java
+++ product/src/main/java/com/yas/product/configurations/SecurityConfig.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtException;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -20,6 +23,11 @@ public class SecurityConfig {
                 .oauth2ResourceServer().jwt();
 
         return http.build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return token -> Jwt.withTokenValue(token).build();
     }
 
     @Bean
