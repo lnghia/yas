c8982e103c0f64a25b64783d4eff69199dd4ff9c
trong nghia <trongnghialedinh@gmail.com>
2022-07-14 16:30:01 +0700

    override some repository methods, add entity graph

--- product/src/main/java/com/yas/product/repository/ProductRepository.java
+++ product/src/main/java/com/yas/product/repository/ProductRepository.java
@@ -2,12 +2,21 @@ package com.yas.product.repository;
 
 import com.yas.product.model.Product;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ProductRepository
-    extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {}
+    extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
+  @Override
+  @EntityGraph(attributePaths = {"brand"})
+  Page<Product> findAll(Pageable pageable);
+
+  @EntityGraph(attributePaths = {"brand", "productCategories", "productCategories.category"})
+  Optional<Product> findById(Long id);
+}
